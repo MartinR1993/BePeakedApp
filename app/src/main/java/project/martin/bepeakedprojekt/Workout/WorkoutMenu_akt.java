04481ba56e6b8bd9f0366f5f3ff47d26e9513049
IamHexor <ykhexor@hotmail.com>
2016-11-28 19:54:53 +0100

    Så kan vi også tilføje workouts.

--- app/src/main/java/project/martin/bepeakedprojekt/Workout/WorkoutMenu_akt.java
+++ app/src/main/java/project/martin/bepeakedprojekt/Workout/WorkoutMenu_akt.java
@@ -1,42 +1,75 @@
 package project.martin.bepeakedprojekt.Workout;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import project.martin.bepeakedprojekt.R;
 
-public class WorkoutMenu_akt extends AppCompatActivity {
+public class WorkoutMenu_akt extends AppCompatActivity  {
+
+    ArrayList<WorkoutElement> list;
+    ArrayList<String> exercises;
+    ListView lv;
+    EditText saveWorkoutName;
+    AlertDialog popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_menu);
         setTitle("Workouts");
-        ArrayList<String> exercises = new ArrayList<>();
+        exercises = new ArrayList<>();
         exercises.add("Benchpress");
         exercises.add("Push Ups");
 
-        ArrayList<WorkoutElement> list = new ArrayList<>();
+        list = new ArrayList<>();
         list.add(new WorkoutElement("Workout 1", exercises, R.drawable.forward));
         list.add(new WorkoutElement("Workout 2", exercises, R.drawable.forward));
 
-        ListView lv = (ListView) findViewById(R.id.listWorkoutMenu);
-        lv.setAdapter(new WorkoutListAdapter(this,list));
+        lv = (ListView) findViewById(R.id.listWorkoutMenu);
+        lv.setAdapter(new WorkoutListAdapter(this, list));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
     }
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId() == android.R.id.home){
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
-        }
-        else if(item.getItemId() == R.id.add){
-            Toast.makeText(this, "Her skal tilføjes et workout", Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == R.id.add) {
+
+            popup = new AlertDialog.Builder(WorkoutMenu_akt.this).create();
+            View createWorkout = View.inflate(this, R.layout.popup_createworkout, null);
+
+
+            saveWorkoutName = (EditText) createWorkout.findViewById(R.id.giveNameWorkout);
+            final Button saveWorkoutButton = (Button) createWorkout.findViewById(R.id.saveWorkout);
+            saveWorkoutButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    list.add(new WorkoutElement(saveWorkoutName.getText().toString(), exercises, R.drawable.forward));
+                    System.out.println("hej"+ list.size());
+                    lv.invalidateViews();
+                    popup.cancel();
+
+                }
+            });
+
+            popup.setTitle("Create Workout");
+            popup.setView(createWorkout);
+            popup.show();
+
+
         }
         return super.onOptionsItemSelected(item);
     }
