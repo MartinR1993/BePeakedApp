a3e436d642a5110595fd256093c4db515a9117e4
IamHexor <ykhexor@hotmail.com>
2017-01-03 15:17:08 +0100

    Så virker noget af funk til tilføj / søg øvelse

--- app/src/main/java/project/martin/bepeakedprojekt/Workout/WorkoutExercises/Workout_Exercises_akt.java
+++ app/src/main/java/project/martin/bepeakedprojekt/Workout/WorkoutExercises/Workout_Exercises_akt.java
@@ -1,19 +1,29 @@
 package project.martin.bepeakedprojekt.Workout.WorkoutExercises;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import project.martin.bepeakedprojekt.Exercises.ExerciseElement;
+import project.martin.bepeakedprojekt.Misc.DummyData;
 import project.martin.bepeakedprojekt.R;
 import project.martin.bepeakedprojekt.Workout.WorkoutElement;
 
 public class Workout_Exercises_akt extends AppCompatActivity {
+
+    private AlertDialog popup;
+    ListView listOfExercises;
+    ArrayList<ExerciseElement> exerciseList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +32,66 @@ public class Workout_Exercises_akt extends AppCompatActivity {
         WorkoutElement workout = (WorkoutElement) getIntent().getSerializableExtra("workout");
         setTitle(workout.getName());
 
-        ArrayList<ExerciseElement> exerciseList = workout.getExercises();
+        exerciseList = workout.getExercises();
 
         ListView lv = (ListView) findViewById(R.id.exerciseList);
         lv.setAdapter(new WorkoutExercisesListAdapter(this, exerciseList));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+
+
+
     }
     public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
         }
-
         else if (item.getItemId() == R.id.add) {
-            Toast.makeText(this, "Her skal tilføjes en exercise", Toast.LENGTH_LONG).show();
+            popup = new AlertDialog.Builder(Workout_Exercises_akt.this).create();
+
+            View addExercise = View.inflate(this, R.layout.popup_addexercise, null);
+
+            EditText searchText = (EditText) addExercise.findViewById(R.id.searchText);
+            searchText.setText("hej");
+
+
+            ArrayList<ExerciseElement> allExercises = new ArrayList<ExerciseElement>();
+
+            listOfExercises = (ListView) addExercise.findViewById(R.id.listViewExer);
+
+
+            for(int i = 1 ; i <= DummyData.exerciseList.length ; i++){
+                allExercises.add(DummyData.getExercise(i));
+                System.out.println(DummyData.getExercise(i).getName());
+           }
+
+                System.out.println(allExercises.size());
+
+            // VIRKER halvt
+             listOfExercises.setAdapter(new WorkoutExercisesListAdapter(this,allExercises));
+
+
+            final Button addExerBotton = (Button) addExercise.findViewById(R.id.addExerBotton);
+            addExerBotton.setText("Add Exercise");
+            addExerBotton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+               //     workoutList.add(new WorkoutElement(-1, saveExerciseName.getText().toString(), new ArrayList<ExerciseElement>()));
+                //    listAdapter.notifyDataSetChanged();
+                    popup.cancel();
+                }
+            });
+
+           // popup.setTitle("Add Exercise");
+            popup.setView(addExercise);
+            popup.show();
         }
         return super.onOptionsItemSelected(item);
-    }
+        }
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.addmenu, menu);
