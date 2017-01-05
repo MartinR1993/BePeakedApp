fc85e3fa5649a11a2603df6b96157308fd9635e4
Martin <Martin_rodgaard@hotmail.dk>
2017-01-05 16:09:24 +0100

    Oprettet language klasse

---
+++ app/src/main/java/project/martin/bepeakedprojekt/Settings/Language_akt.java
@@ -0,0 +1,15 @@
+package project.martin.bepeakedprojekt.Settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import project.martin.bepeakedprojekt.R;
+
+public class Language_akt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_akt);
+    }
+}
