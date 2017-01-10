cde097b8b9ed28269691259bc773edb5bd06f1fe
IamHexor <ykhexor@hotmail.com>
2017-01-10 15:57:55 +0100

    noget mere kunde / ikke kunde

--- app/src/main/java/project/martin/bepeakedprojekt/Settings/Settings_akt.java
+++ app/src/main/java/project/martin/bepeakedprojekt/Settings/Settings_akt.java
@@ -3,6 +3,7 @@ package project.martin.bepeakedprojekt.Settings;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import project.martin.bepeakedprojekt.Logind_akt;
 import project.martin.bepeakedprojekt.R;
 
@@ -19,7 +22,15 @@ public class Settings_akt extends AppCompatActivity implements AdapterView.OnIte
 
     SharedPreferences prefs;
     String language;
-    String unitsystem;
+    ArrayList<String> indstillinger;
+    String profil;
+    String activationKey;
+    String changeLanguage;
+    String unitSystem;
+    String about;
+    String credits;
+    String logOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +61,43 @@ public class Settings_akt extends AppCompatActivity implements AdapterView.OnIte
 //            unitsystem = "Imperial";
 //        }
 
-        String[] indstillinger = {getString(R.string.settings_profile), getString(R.string.settings_language) + "  -  " + language , getString(R.string.settings_unitsystem), getString(R.string.settings_about), getString(R.string.credits_title), getString(R.string.settings_logout)};
+
+        profil = getString(R.string.settings_profile);
+        activationKey = "ActivationKey";
+        changeLanguage = getString(R.string.settings_language) + "  -  " + language;
+        unitSystem = getString(R.string.settings_unitsystem);
+        about = getString(R.string.settings_about);
+        credits = getString(R.string.credits_title);
+        logOut = getString(R.string.settings_logout);
+
+
+
+        String[] indstillinger = {
+                getString(R.string.settings_profile),
+                getString(R.string.settings_language) + "  -  " + language,
+                getString(R.string.settings_unitsystem),
+                getString(R.string.settings_about),
+                getString(R.string.credits_title),
+                getString(R.string.settings_logout)
+        };
+
+        if(prefs.getInt("usertype",0) == 1)
+            indstillinger = new String[]{
+                    getString(R.string.settings_profile),
+                    "ActivationKey",
+                    getString(R.string.settings_language) + "  -  " + language,
+                    getString(R.string.settings_unitsystem),
+                    getString(R.string.settings_about),
+                    getString(R.string.credits_title),
+                    getString(R.string.settings_logout)
+            };
+
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_selectable_list_item, android.R.id.text1, indstillinger);
 
         listView = (ListView) findViewById(R.id.listView);
         listView.setOnItemClickListener(this);
         listView.setAdapter(adapter);
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -72,37 +114,39 @@ public class Settings_akt extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        switch (position) {
-            case 0: {
-                //TODO: her skal der oprettes en profil side
-                break;
-            }
-            case 1: {
-                Intent i = new Intent(this, Language_akt.class);
-                startActivity(i);
-                break;
-            }
-            case 2: {
-                Intent i = new Intent(this, UnitSystem_akt.class);
-                startActivity(i);
-                break;
-            }
-            case 3: {
-                //TODO: her skal der oprettes en om side
-                break;
-            }
-            case 4: {
-                Intent i = new Intent(this, Credits_akt.class);
-                startActivity(i);
-                break;
-            }
-            case 5: {
-                Intent i = new Intent(this, Logind_akt.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(i);
-                break;
-            }
-            default: break;
-        }
+
+       String data= (String)parent.getItemAtPosition(position);
+
+if (data.equalsIgnoreCase(profil)){
+
+}else if (data.equalsIgnoreCase(activationKey)){
+
+    System.out.println("lol");
+    Intent i = new Intent(this, ActivationKey_akt.class);
+    startActivity(i);
+
+
+}else if (data.equalsIgnoreCase(changeLanguage)){
+    Intent i = new Intent(this, Language_akt.class);
+    startActivity(i);
+
+}else if (data.equalsIgnoreCase(unitSystem)){
+    Intent i = new Intent(this, UnitSystem_akt.class);
+    startActivity(i);
+
+}else if (data.equalsIgnoreCase(about)){
+
+}else if (data.equalsIgnoreCase(credits)){
+    Intent i = new Intent(this, Credits_akt.class);
+    startActivity(i);
+
+}else if (data.equalsIgnoreCase(logOut)){
+    Intent i = new Intent(this, Logind_akt.class);
+    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    startActivity(i);
+}
+
     }
+
+
 }
