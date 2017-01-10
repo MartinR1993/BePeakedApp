8b9d870374d2e053d49409f0b51083f4d6aa3d24
kvisgaard96 <mathias@kvisgaard.info>
2017-01-10 13:10:11 +0100

    Lavet about og fikset mere sprog i values strings

---
+++ app/src/main/java/project/martin/bepeakedprojekt/Settings/About_akt.java
@@ -0,0 +1,34 @@
+package project.martin.bepeakedprojekt.Settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import project.martin.bepeakedprojekt.R;
+
+public class About_akt extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_akt);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle(getString(R.string.credits_title));
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
