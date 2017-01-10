7388fad6b0872ec64d920cc757e7f3fc3a3f6344
IamHexor <ykhexor@hotmail.com>
2017-01-10 12:19:12 +0100

    Nu kan vi begynde at kende forskel på kunde og ikke kunde. midlertidigt bruges opret bruger til at gøre at et login bliver som ikke kunde.

--- app/src/main/java/project/martin/bepeakedprojekt/Logind_akt.java
+++ app/src/main/java/project/martin/bepeakedprojekt/Logind_akt.java
@@ -1,7 +1,9 @@
 package project.martin.bepeakedprojekt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +15,17 @@ public class Logind_akt extends AppCompatActivity implements View.OnClickListene
     private TextView textUsername, textPassword, opretBruger;
     private EditText editUsername, editPassword;
     private Button buttonLogin;
+    SharedPreferences prefs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_akt);
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.edit().putInt("usertype",0).commit();
         setTitle(R.string.login_banner);
 
         textUsername = (TextView) findViewById(R.id.textUsername);
@@ -37,11 +44,18 @@ public class Logind_akt extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         if(v == buttonLogin){
+
+
+
             Intent i = new Intent(this, MainMenu_akt.class);
             Logind_akt.this.finish();
             startActivity(i);
         }
         else if (v == opretBruger){
+
+            // bruges midlertidig til at sætte en bruger til ikke kunde
+            prefs.edit().putInt("usertype",1).commit();
+
             System.out.println("Opret bruger");
         }
     }
