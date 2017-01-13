2053fb1f5df71c6beb828c92ca243c271e8ee343
kvisgaard96 <mathias@kvisgaard.info>
2017-01-13 13:12:19 +0100

    Gjort klar til "skift kodeord"

--- app/src/main/java/project/martin/bepeakedprojekt/Settings/User_akt.java
+++ app/src/main/java/project/martin/bepeakedprojekt/Settings/User_akt.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import project.martin.bepeakedprojekt.R;
 
-public class User_akt extends AppCompatActivity {
+public class User_akt extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView skiftKode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,11 @@ public class User_akt extends AppCompatActivity {
         setContentView(R.layout.activity_user_akt);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(getString(R.string.settings_profile));
+
+
+        skiftKode = (TextView) findViewById(R.id.changepassword);
+        skiftKode.setOnClickListener(this);
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,5 +38,14 @@ public class User_akt extends AppCompatActivity {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+     public void onClick(View v) {
+        if (v == skiftKode){
+            //TODO
+            //Databasekald
+            // implementere en metode der skifter adganskode, og åbner et popup vindue hvori alt dette kommer til at foregå
+            Toast.makeText(getApplicationContext(), "Mangler kode hertil", Toast.LENGTH_LONG).show();
+        }
     }
 }
